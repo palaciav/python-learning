91cbdbd700806ffa49b8caee27166c78701c8944
Adrian Palacios <palaciav@gmail.com>
2018-02-21 21:57:32 -0800

    Update app.py

--- Udemy/Python Mega Course/Demonstrations/Interactive Dictionary/app.py
+++ Udemy/Python Mega Course/Demonstrations/Interactive Dictionary/app.py
@@ -8,7 +8,13 @@ def return_def(query):
     if query in data:
         return(data[query])
     elif len(get_close_matches(query, data.keys())) > 0:
-        return "Did you mean %s instead?" % get_close_matches(query, data.keys())[0]
+        yn = input("Did you mean %s instead?" % get_close_matches(query, data.keys())[0])
+        if yn.lower() == "y" or yn.lower() == "yes":
+            return data[get_close_matches(query, data.keys())[0]]
+        elif yn.lower() == "n" or yn.lower() == "no":
+            return "The word does not exist, please double check it."
+        else:
+            return "I didn't understand your response."
     else:
         return "The word does not exist, please double check."
 
