d6f5c9a211679f190bc97e10cb0fdd48667060ef
Adrian Palacios <palaciav@gmail.com>
2020-03-28 17:01:09 -0700

    Adding Devops code

---
+++ PythonForDevops/Ch3-CommandLine/example3-7.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+A simple plugin discovery script
+"""
+import fire
+import pkgutil
+import importlib
+
+def find_and_run_plugins(plugin_prefix):
+    plugins = {}
+
+    # Discover and Load plugins
+    print(f"Discovering plugins with prefix: {plugin_prefix}")
+    for _, name, _ in pkgutil.iter_modules():
+        if name.startswith(plugin_prefix):
+            module = importlib.import_module(name)
+            plugins[name] = module
+    # Run plugins
+    for name, module in plugins.items():
+        print(f"Running plugin {name}")
+        module.run()
+
+if __name__ == "__main__":
+    fire.Fire()
